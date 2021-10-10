1179156d795af5083a7f4a6a33366a815d4bd5dc
ss530eh <samuel.sirotnik@student.tuke.sk>
2021-10-10 15:47:22 +0200

    adding the date

---
+++ app.js
@@ -0,0 +1,19 @@
+const date = document.getElementById("date");
+
+function setDate() {
+  var date = new Date();
+
+  var day = date.getDate();
+  var month = date.getMonth() + 1;
+  var year = date.getFullYear();
+
+  if (month < 10) month = "0" + month;
+  if (day < 10) day = "0" + day;
+
+  var today = year + "-" + month + "-" + day;
+  return today;
+}
+
+setTimeout(setDate, 1000);
+
+date.innerText = setDate();
